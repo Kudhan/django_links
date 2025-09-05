1099b1d8d92d927550f941b7f1d8114a4a1cbbe2
Kudhan <kudhanshaik03@gmail.com>
2025-09-05 16:42:39 +0530

    First commit

---
+++ links/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Link
+
+def index(request):
+    links = Link.objects.all()
+    context = {
+        'links': links
+    }
+    return render(request, 'links/index.html', context)
+
+def root_link(request, link_slug):
+    link = get_object_or_404(Link, slug=link_slug)
+    link.increment_clicks()   # <-- fixed method name
+    return redirect(link.url)
