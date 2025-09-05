392d2b2d1a07b83657e43790a2de18c585d73376
Kudhan <kudhanshaik03@gmail.com>
2025-09-05 17:34:14 +0530

    added modelform

--- links/views.py
+++ links/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Link
+from .forms import LinkForm
 
 def index(request):
     links = Link.objects.all()
@@ -15,6 +16,19 @@ def root_link(request, link_slug):
 
 
 def add_links(request):
-    print(request.POST) 
-    print(request.data)
-    return render(request,'links/create.html',{})
+    if request.method == 'POST':
+        form = LinkForm(request.POST)
+        if form.is_valid():
+            
+            form.save()
+            return redirect('home')
+        else:
+            print("FORM ERRORS:", form.errors)
+    else:
+        form = LinkForm()
+
+    return render(request, 'links/create.html', {"form": form})
+
+
+    
+    
